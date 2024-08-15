4d404b9ff39e126479329abb20f0bda09afb2448
Michael Thomas <michaelhthomas@outlook.com>
2024-08-14 21:53:44 -0400

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-red-500">Hello there</h1>
+    </div>
+  );
+};
+
+export default App;
