026974aa1258c32d6c13f234d23a03d441183651
Michael Thomas <michaelhthomas@outlook.com>
2024-08-16 13:10:29 -0400

    fix(vite): do not minify Vue compiler errors in production builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import replace from "@rollup/plugin-replace";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
 	plugins: [
+		// provide unminified Vue compiler errors
+		replace({
+			"process.env.NODE_ENV": JSON.stringify("development"),
+			include: "**/@vue[/+]compiler-*/**/*.js",
+			preventAssignment: true,
+		}),
 		/* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
