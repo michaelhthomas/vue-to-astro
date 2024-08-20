4b35fc2fd2dd190e1e89c2c62217fc1f7a9051b5
Michael Thomas <michaelhthomas@outlook.com>
2024-08-20 10:11:47 -0400

    feat(MainLayout): add github icon to navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,20 @@
 import { ParentComponent } from "solid-js";
+import { RiLogosGithubFill } from "solid-icons/ri";
 
 export const MainLayout: ParentComponent<{ title: string }> = (props) => {
 	return (
 		<div class="flex h-full flex-col">
 			<div class="navbar bg-base-200">
-				<a class="px-4 text-xl font-semibold">{props.title}</a>
+				<a class="navbar-start px-4 text-xl font-semibold">{props.title}</a>
+
+				<a
+					class="navbar-end px-4"
+					href="https://github.com/michaelhthomas/vue-to-astro"
+					aria-label="Github Project"
+					target="_blank"
+				>
+					<RiLogosGithubFill class="size-6" />
+				</a>
 			</div>
 
 			<div class="min-h-0 w-full flex-grow">{props.children}</div>
